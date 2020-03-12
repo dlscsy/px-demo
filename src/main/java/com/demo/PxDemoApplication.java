0de91fa4e39523e076908ca247a45a93e05cdd1d
silent <silentoopm@163.com>
2020-03-12 09:44:24 +0800

    Initial commit

---
+++ src/main/java/com/demo/PxDemoApplication.java
@@ -0,0 +1,27 @@
+package com.demo;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@MapperScan("com.demo.mapper")
+public class PxDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PxDemoApplication.class, args);
+    }
+
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**");
+//            }
+//        };
+//    }
+}
